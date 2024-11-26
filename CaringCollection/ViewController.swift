0dfb7d16246b86e5656c214233c135aee681065c
Marat Nazirov <nazimovma@sovcombank.ru>
2024-11-26 22:30:37 +0300

    common init

---
+++ CaringCollection/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Created by Marat Nazirov on 26.11.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        commonInit()
+    }
+}
+
+// MARK: - Private methods
+
+private extension ViewController {
+    func commonInit() {
+        view.backgroundColor = .white
+    }
+}
